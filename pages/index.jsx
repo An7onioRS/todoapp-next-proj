ffa098f633c8a9275c86480960a6d9991500ffaa
Antonio Mansory <antoniosimeonov7@gmail.com>
2023-01-23 16:23:24 +0200

    Add create new todo functionality

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,12 @@
 import Todo from '@/components/Todo'
-import { collection, onSnapshot, query, updateDoc, doc } from 'firebase/firestore'
+import { 
+        collection, 
+        onSnapshot, 
+        query, 
+        updateDoc, 
+        doc,
+        addDoc
+      } from 'firebase/firestore'
 import { db } from './firebase'
 import Head from 'next/head'
 import Image from 'next/image'
@@ -9,8 +16,23 @@ import { AiOutlinePlus } from 'react-icons/ai'
 export default function Home() {
 
   const [todos, setTodos] = useState([])
+  const [input, setInput] = useState('')
 
   // Create todo
+  const createTodo = async e => {
+    e.preventDefault()
+    
+    if (!input) {
+      alert('Todos cannot be empty! :)')
+      return
+    }
+
+    await addDoc(collection(db, 'asdfqwe'), {
+      text: input,
+      completed: false,
+    })
+    setInput('')
+  }
 
    // Read todo from firebase
   useEffect(() => {
@@ -47,8 +69,8 @@ export default function Home() {
       <div className='h-screen w-screen p-4 bg-gradient-to-r from-[#2f80ed] to-[#1cb5e0]'>
         <div className='bg-slate-100 max-w-[500px] w-full m-auto rounded-md p-4 shadow-xl'>
           <h3 className='text-3xl font-bold text-center text-gray-800 p-2'>To Do App</h3>
-          <form className='flex justify-between gap-2'>
-            <input className='border p-2 w-full text-xl' type="text" placeholder='Add Todo' />
+          <form onSubmit={createTodo} className='flex justify-between gap-2'>
+            <input value={input} onChange={e => setInput(e.target.value)} className='border p-2 w-full text-xl' type="text" placeholder='Add Todo' />
             <button className='border p-4 bg-purple-500 text-slate-100'><AiOutlinePlus size={30} /></button>
           </form>
           <ul>
@@ -57,7 +79,7 @@ export default function Home() {
               }/>)
             }
           </ul>
-          <p className='text-center p-2'>You have {todos.length} todos.</p>
+         {todos.length < 1 ? null : <p className='text-center p-2'>You have {todos.length} todos.</p>}
         </div>
       </div>
     </>
