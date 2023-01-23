5b186bc80943581035dc4bf9abcceee49be37313
Antonio Mansory <antoniosimeonov7@gmail.com>
2023-01-23 12:04:26 +0200

    Add styles

---
+++ components/Todo.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { FaRegTrashAlt } from 'react-icons/fa'  
+
+const Todo = ({ todo }) => {
+
+  return (
+    <li className='flex justify-between items-center gap-4 bg-slate-200 p-4 my-2 capitalize'>
+    {/* if li complete => bg-slate-400 */}
+            <div className='flex items-center gap-4'>
+                <input className='cursor-pointer' type="checkbox" />
+                {/* if input complete => line-through */}
+                <p className=''>{todo}</p>
+            </div>
+            <button className='cursor-pointer flex items-center'><FaRegTrashAlt size={20} /></button>
+    </li>
+  )
+}
+
+export default Todo
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
+import Todo from '@/components/Todo'
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { useState } from 'react'
+import { AiOutlinePlus } from 'react-icons/ai'
 
 export default function Home() {
+
+  const [todos, setTodos] = useState(['Learn React', 'Start Trading Professionally', 'Read 10 pages of Think and Grow Rich'])
+
   return (
     <>
       <Head>
@@ -14,8 +17,21 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className='underline underline-offset-1'>Hey there!</h1>
-      
+      <div className='h-screen w-screen p-4 bg-gradient-to-r from-[#2f80ed] to-[#1cb5e0]'>
+        <div className='bg-slate-100 max-w-[500px] w-full m-auto rounded-md p-4 shadow-xl'>
+          <h3 className='text-3xl font-bold text-center text-gray-800 p-2'>To Do App</h3>
+          <form className='flex justify-between gap-2'>
+            <input className='border p-2 w-full text-xl' type="text" placeholder='Add Todo' />
+            <button className='border p-4 bg-purple-500 text-slate-100'><AiOutlinePlus size={30} /></button>
+          </form>
+          <ul>
+            {
+              todos.map((todo, index) => <Todo key={index} todo={todo} />)
+            }
+          </ul>
+          <p className='text-center p-2'>You have {todos.length} todos.</p>
+        </div>
+      </div>
     </>
   )
 }
